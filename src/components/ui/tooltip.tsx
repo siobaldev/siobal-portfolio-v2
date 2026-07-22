43f276743a447ab5fc5341dcff07716b40d95aa5
siobal <sblmnrd@gmail.com>
2026-07-23 04:50:58 +0800

    feat: add header sub-components (tooltip, desktop nav, greeting)

---
+++ src/components/ui/tooltip.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  title: string;
+  position: string;
+};
+
+export default function Tooltip({ title, position }: Props) {
+  return (
+    <span
+      className={`absolute whitespace-nowrap border-2 border-border-muted bg-header-bg ${position} left-1/2 z-20 -translate-x-1/2 scale-0 transform rounded-lg px-4 py-2 font-medium text-foreground text-sm shadow-lg transition-transform duration-300 ease-in-out group-hover:scale-100`}
+    >
+      {title}
+    </span>
+  );
+}
