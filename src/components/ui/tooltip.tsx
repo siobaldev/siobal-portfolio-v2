d94a65043eb5af288fd1e14e31eab204ab745386
siobal <sblmnrd@gmail.com>
2026-07-25 10:40:31 +0800

    fix: adjust tooltip background for better visibility

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function Tooltip({ title, position }: Props) {
   return (
     <span
-      className={`absolute whitespace-nowrap border-2 border-border-muted bg-header-bg ${position} left-1/2 z-20 -translate-x-1/2 scale-0 transform rounded-lg px-4 py-2 font-medium text-foreground text-sm shadow-lg transition-transform duration-300 ease-in-out group-hover:scale-100`}
+      className={`absolute whitespace-nowrap border-2 border-border-muted bg-border ${position} left-1/2 z-20 -translate-x-1/2 scale-0 transform rounded-lg px-4 py-2 font-medium text-[#F0EEE9] text-sm shadow-lg transition-transform duration-300 ease-in-out group-hover:scale-100`}
     >
       {title}
     </span>
